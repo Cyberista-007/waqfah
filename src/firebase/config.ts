2e23d2cdb021a16726f873198fbf785b96f42538
Abdo Reda <abdoreda6249@gmail.com>
2025-11-16 19:32:14 +0000

    Try fixing this error: `Console Error: [2025-11-16T19:30:39.487Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:698667694166:web:b46b59176030753ab7a3f7",
   "apiKey": "AIzaSyBNGvKogSh0ASEWf0hG5yUTaVD1b6J2ED4",
   "authDomain": "studio-2429347890-4a52c.firebaseapp.com",
-  "measurementId": "",
+  "storageBucket": "studio-2429347890-4a52c.appspot.com",
   "messagingSenderId": "698667694166"
 };
