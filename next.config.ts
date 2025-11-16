48ab59883e61a15d458f17cad871e05d9cdc5fbd
Abdo Reda <abdoreda6249@gmail.com>
2025-11-16 16:21:38 +0000

    عند الضغط علي اي زر في صفحة مدير التحكم تظهر هذصفحة تسجبيل دخول المدير ا

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
