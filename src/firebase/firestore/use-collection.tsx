85959e2413fa010b6d853ea4d24a1a0e91726a44
Abdo Reda <abdoreda6249@gmail.com>
2025-11-18 18:11:48 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Expected first arg

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -61,7 +61,7 @@ export function useCollection<T = any>(
   type StateDataType = ResultItemType[] | null;
 
   const [data, setData] = useState<StateDataType>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false); 
+  const [isLoading, setIsLoading] = useState<boolean>(true); 
   const [error, setError] = useState<FirestoreError | Error | null>(null);
 
   // Use a stable reference for the query across renders
